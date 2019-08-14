553c9c6d06b7726df92ccf11057a5aa90e34fd76
Slowpoke <48594426+Slowpoke42@users.noreply.github.com>
2019-08-14 16:11:54 -0700

    state

--- include/gui/graph_widget/items/graphics_node.h
+++ include/gui/graph_widget/items/graphics_node.h
@@ -23,8 +23,8 @@ public:
 
     graphics_node(const hal::item_type type, const u32 id, const QString& name);
 
-    virtual QRectF boundingRect() const Q_DECL_OVERRIDE;
-    virtual QPainterPath shape() const Q_DECL_OVERRIDE;
+    virtual QRectF boundingRect() const override;
+    virtual QPainterPath shape() const override;
 
     virtual QPointF get_input_scene_position(const u32 net_id, const QString& pin_type) const = 0;
     virtual QPointF get_output_scene_position(const u32 net_id, const QString& pin_type) const = 0;
@@ -41,7 +41,7 @@ public:
 //    qreal y_offset() const;
 
 protected:
-    QVariant itemChange(GraphicsItemChange change, const QVariant& value) Q_DECL_OVERRIDE;
+    QVariant itemChange(GraphicsItemChange change, const QVariant& value) override;
 
     QString m_name;
 
