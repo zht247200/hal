98febae1999f74eed97ed89cd60dee49e7c5d78f
Nils Albartus <nils.albartus@rub.de>
2020-01-13 17:59:18 +0100

    working sat solver integration

--- cmake/FindZ3.cmake
+++ cmake/FindZ3.cmake
@@ -1,27 +1,72 @@
-# Find Z3
+# - Try to find Z3
+# Once done this will define
+#  Z3_FOUND - System has Z3
+#  Z3_INCLUDE_DIRS - The Z3 include directories
+#  Z3_LIBRARIES - The libraries needed to use Z3
 
-set(Z3_ROOT "" CACHE PATH "Root of Z3 distribution.")
-find_path(Z3_INCLUDE_DIR NAMES z3.h z3++.h PATHS ${Z3_ROOT}/include)
-find_library(Z3_LIBRARY NAMES z3 PATHS ${Z3_ROOT}/lib)
 
-mark_as_advanced(Z3_EXECUTABLE Z3_INCLUDE_DIR Z3_LIBRARY)
-find_program (Z3_EXECUTABLE
-  NAMES z3 PATHS ${Z3_ROOT} PATH_SUFFIXES bin 
-  DOC "z3 command line executable")
-mark_as_advanced(Z3_EXECUTABLE)
+find_path(Z3_INCLUDE_DIR z3++.h)
+find_library(Z3_LIBRARY z3)
 
-if (Z3_EXECUTABLE)
-  execute_process (COMMAND ${Z3_EXECUTABLE} -version
-    OUTPUT_VARIABLE z3_version
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  if (z3_version MATCHES "^Z3 version [0-9]")
-    string (REPLACE "Z3 version " "" Z3_VERSION_STRING ${z3_version})
+# If library found, check the version
+if (Z3_INCLUDE_DIR AND Z3_LIBRARY)
+
+  # Check version from char *msat_get_version(void)
+  file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.cpp" "
+    #include <stdio.h>
+    #include \"z3.h\"
+
+    int main() {
+      unsigned major, minor, build_number, revision_number;
+      Z3_get_version(&major, &minor, &build_number, &revision_number);
+      printf(\"%u.%u.%u.%u\\n\", major, minor, build_number, revision_number);
+      return 0;
+    }
+  ")
+
+  # Run the test program
+  try_run(
+    VERSION_TEST_EXITCODE
+    VERSION_TEST_COMPILED
+    ${CMAKE_BINARY_DIR}
+    ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.cpp
+    COMPILE_DEFINITIONS
+      -I"${Z3_INCLUDE_DIR}"
+      LINK_LIBRARIES ${Z3_LIBRARY} gmp
+    CMAKE_FLAGS
+      -DCMAKE_SKIP_RPATH:BOOL=${CMAKE_SKIP_RPATH}
+    RUN_OUTPUT_VARIABLE
+      VERSION_TEST_RUN_OUTPUT
+  )
+
+  if (NOT VERSION_TEST_COMPILED)
+    unset(Z3_INCLUDE_DIR CACHE)
+    unset(Z3_LIBRARY CACHE)
+  elseif (NOT ("${VERSION_TEST_EXITCODE}" EQUAL 0))
+    unset(Z3_INCLUDE_DIR CACHE)
+    unset(Z3_LIBRARY CACHE)
+  else()
+    # Output is of the form: major.minor.build_number.revision
+    if("${VERSION_TEST_RUN_OUTPUT}" MATCHES "([0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*)")
+      set(Z3_VERSION "${CMAKE_MATCH_1}")
+      if ("${Z3_VERSION}" VERSION_LESS "${Z3_FIND_VERSION}")
+    	unset(Z3_INCLUDE_DIR CACHE)
+    	unset(Z3_LIBRARY CACHE)
+      elseif (Z3_FIND_VERSION_EXACT AND NOT "${Z3_VERSION}" VERSION_EQUAL "${Z3_FIND_VERSION}")
+    	unset(Z3_INCLUDE_DIR CACHE)
+     	unset(Z3_LIBRARY CACHE)
+      endif()
+    else()
+      unset(Z3_INCLUDE_DIR CACHE)
+      unset(Z3_LIBRARY CACHE)
+    endif()
   endif()
 endif()
-mark_as_advanced(Z3_VERSION_STRING)
 
-# include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Z3
-  REQUIRED_VARS Z3_LIBRARY Z3_INCLUDE_DIR Z3_EXECUTABLE
-  VERSION_VAR Z3_VERSION_STRING)
+set(Z3_LIBRARIES ${Z3_LIBRARY})
+set(Z3_INCLUDE_DIRS ${Z3_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Z3 DEFAULT_MSG Z3_LIBRARY Z3_INCLUDE_DIR)
+
+mark_as_advanced(Z3_INCLUDE_DIR Z3_LIBRARY)
